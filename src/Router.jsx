9a181e1ead74bef0d6f540cd17b949d531504079
jokay03J <magrojoan3@gmail.com>
2023-02-09 22:28:13 +0100

    update router
    *add getting started page & components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,20 @@
 import { RouterProvider } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import Components from "./pages/Components";
+import GettingStarted from "./pages/GettingStarted";
 import Home from "./pages/Home";
 
 function Router() {
   const routes = createBrowserRouter([
     { path: `${import.meta.env.VITE_PUBLIC_BASE_URL}/`, element: <Home /> },
+    {
+      path: `${import.meta.env.VITE_PUBLIC_BASE_URL}/getting-started`,
+      element: <GettingStarted />,
+    },
+    {
+      path: `${import.meta.env.VITE_PUBLIC_BASE_URL}/components`,
+      element: <Components />,
+    },
   ]);
   return <RouterProvider router={routes}></RouterProvider>;
 }
