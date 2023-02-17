4b5495145c79d3350443f04aac0182374e2df2d8
jokay03J <magrojoan3@gmail.com>
2023-02-17 19:05:44 +0100

    add code exemple

---
+++ src/utils/exemplecode.js
@@ -0,0 +1,84 @@
+export const NavbarCodeExemple = `<nav class="navbar--red text--white mb-4">
+    <div class="container">
+        <h2 class="site--title">Shinobi CSS</h2>
+        <label class="navbar--mobile--btn" for="navbar--input">Menu</label>
+        <input
+            type="checkbox"
+            name="navbar--input"
+            id="navbar--input"
+            class="navbar--mobile--checked"
+        />
+        <ul class="items">
+            <li class="item">Home</li>
+            <li class="item">Getting Started</li>
+            <li class="item">Components</li>
+        </ul>
+    </div>
+</nav>`;
+
+export const ColorsExemple = `<h1 class="text--orange">texte en orange</h1>
+<h1 class="bg--black text--white">fond noir, texte en blanc</h1>
+<h1 class="text--green text--hover--red">
+  texte en vert et au hover en rouge
+</h1>`;
+
+export const CardExemple = `<div class="card w--2-6 my--1 mx--1">
+<h2 class="card--title">card title</h2>
+<p class="card--body">card body</p>
+</div>`;
+
+export const ButtonExemple = `<button class="btn">default</button>
+<button class="btn--primary">primary</button>
+<button class="btn--secondary">secondary</button>
+<button class="btn--orange">orange</button>`;
+
+export const GridSystemExemple = `<div class="row gap-2 justify--center">
+<div class="col-12-xs col-5-sm col-3-xl">
+  <div class="card">
+    <h3 class="card--title">Hello, ninjas</h3>
+    <p class="card--body">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    </p>
+  </div>
+</div>
+<div class="col-12-xs col-5-sm col-3-xl">
+  <div class="card">
+    <h3 class="card--title">Hello, ninjas</h3>
+    <p class="card--body">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    </p>
+  </div>
+</div>
+<div class="col-12-xs col-5-sm col-3-xl">
+  <div class="card">
+    <h3 class="card--title">Hello, ninjas</h3>
+    <p class="card--body">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    </p>
+  </div>
+</div>
+<div class="col-12-xs col-5-sm col-3-xl">
+  <div class="card">
+    <h3 class="card--title">Hello, ninjas</h3>
+    <p class="card--body">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    </p>
+  </div>
+</div>
+</div>`;
+
+export const BorderExemple = `<div class="border--1 border--black border--solid">
+border 1 black solid
+</div>
+<div class="border--2 border--orange border--dashed">
+border 2 orange dashed
+</div>
+<div class="border--3 border--red border--dotted">
+border 3 orange dashed
+</div>
+<div class="border--3 border--green border--outset rounded--sm">
+border 3 orange ouset rounded sm
+</div>`;
+
+export const WidthHeightExemple = `<div class="bg--red w--3-6">50% width</div>
+<div class="bg--green w--9-12 h--5">75% width & h--5</div>`;
