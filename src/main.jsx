30a19c6e33f712cfad93f83099ae7053eda657e7
jokay03J <magrojoan3@gmail.com>
2023-03-08 16:16:05 +0100

    update entry point
    * remove shinobie import and replace it to @jokay03j-v2/sasslib

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
-import "shinobie";
+import "@jokay03j-v2/sasslib";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
